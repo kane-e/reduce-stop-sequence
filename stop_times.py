72b2afa7e09041b485e8899452ec4dc7a4ea2b45
kane-e <68760965+kane-e@users.noreply.github.com>
2023-07-04 22:17:28 -0700

    updated print outputs

--- stop_times.py
+++ stop_times.py
@@ -4,18 +4,18 @@ import csv
 import io
 
 COLOR_RED   = "\033[1;31m"
-COLOR_BLUE  = "\033[1;34m"
 COLOR_GREEN = "\033[0;32m"
 COLOR_RESET = "\033[0;0m"
 
 def convert_stop_seq():
-    filepath = sys.argv[1]
     if len(sys.argv) < 2:
         print(COLOR_RED + "No filename provided!" + COLOR_RESET)
-        exit()
+        exit()    
+    filepath = sys.argv[1]
     if not os.path.isfile(filepath):
-        print(COLOR_RED + "File does not exist!" + COLOR_RESET)
-    if not os.path.basename(filepath) == 'stop_times.txt':
+        print(COLOR_RED + "Input is not a file! Please input stop_times.txt." + COLOR_RESET)
+        exit()
+    if not os.path.basename(filepath) == "stop_times.txt":
         print(COLOR_RED + "Incorrect file input! Please input stop_times.txt" + COLOR_RESET)
         exit()
     make_new_file(filepath)
@@ -26,12 +26,15 @@ def make_new_file(filepath):
         csv_file = csv.DictReader(stop_times_txt)
         csv_list = list(csv_file)
         for row in csv_list:
-            if "stop_sequence" not in row or not row["stop_sequence"]:
-                print(COLOR_BLUE + "No stop_sequence values are present and therefore the operation cannot be performed" + COLOR_RESET)
+            if "stop_sequence" not in row:
+                print(COLOR_RED + "No stop_sequence column detected. The operation cannot be performed." + COLOR_RESET)
                 return
-        file_name = 'stop_times2.txt'
+            if not row["stop_sequence"]:
+                print(COLOR_RED + "Missing stop_sequence value(s). The operation cannot be performed." + COLOR_RESET)
+                return
+        file_name = "stop_times2.txt"
         if os.path.exists(file_name):
-            print(COLOR_RED + "File with name " + file_name + " already exists in directory; cannot create a new one. Move this file and try again. Skipping..." + COLOR_RESET)
+            print(COLOR_RED + "File with name " + file_name + " already exists in directory; cannot create a new one. Move this file and try again." + COLOR_RESET)
             return
         new_file = open(file_name, "w")
         new_csv_writer = csv.DictWriter(new_file, fieldnames=csv_file.fieldnames)
@@ -40,7 +43,7 @@ def make_new_file(filepath):
             row["stop_sequence"] = int(row["stop_sequence"]) - 1
             new_csv_writer.writerow(row)
         new_file.close()
-        print(COLOR_GREEN + "Exported " + file_name + " with stop_sequence reduced by 1" + COLOR_RESET)
+        print(COLOR_GREEN + "Exported " + file_name + " with stop_sequence reduced by 1." + COLOR_RESET)
 
 if __name__ == "__main__":
     convert_stop_seq()
